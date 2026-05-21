73b222cf9b0d99c86c121a0a0262f7be3fa24171
Nishant kaira <kairanishant23@gmail.com>
2026-05-21 18:13:58 +0530

    Delete src directory

--- src/models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require("./User");
-const Product = require("./Product");
-const Order = require("./Order");
-const Contact = require("./Contact");
-const OtpVerification = require("./OtpVerification");
-
-module.exports = { User, Product, Order, Contact, OtpVerification };
